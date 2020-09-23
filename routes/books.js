aa703bb06e4f159c187d7260490675f60625a290
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-09-23 10:02:10 -0700

    why wont it update

--- routes/books.js
+++ routes/books.js
@@ -22,12 +22,12 @@ router.get('/', asyncHandler(async (req, res) => {
 }));
 
 /* GET sorts book by ascending title  */
-router.get('/title/asc', asyncHandler(async (req, res) => {
-  let books = await Book.findAll({
-    order: [['title', 'ASC']]
-  })
-  res.render("books/index", { books, title: "The StarField Library" })
-}))
+// router.get('/title/asc', asyncHandler(async (req, res) => {
+//   let books = await Book.findAll({
+//     order: [['title', 'ASC']]
+//   })
+//   res.render("books/index", { books, title: "The StarField Library" })
+// }))
 
 /* GET shows the create new book form */ 
 router.get('/new', (req, res) => {
@@ -36,26 +36,33 @@ router.get('/new', (req, res) => {
 
 /* POST posts a new book to the database */
 router.post('/', asyncHandler(async (req, res) => {
-  // req body property returns an object containing the key value pairs 
   const book = await Book.create(req.body) 
-  console.log(req.body) // this never logs to console.
-  res.redirect("books/" + book.id);
-}))
+  res.redirect("books");
+}));
 
 /* GET shows book detail form */
 router.get('/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id)
-  res.render("books/detail", { book, title: "Update Book" })
+  if(book) {
+    res.render("books/detail", { book, title: "Update Book" })
+  } else {
+    res.sendStatus(404)
+  }
 }));
 
 /* POST Update book info in the database */
-router.post('/:id', asyncHandler(async(req, res) => {
-  const book = await Book.findByPk(req.params.id);
+router.post('/books/:id', asyncHandler(async(req, res) => {
+  let book;
+  book = await Book.findByPk(req.params.id);
   await book.update(req.body);
-  console.log("log: " + req.body)
-  res.redirect("/books")
+  res.redirect("/books/" + book.id)
 }));
 
 /* POST Delete a book, /books/:id/delete */
+// router.post('/:id', asyncHandler(async (req, res) => {
+//   const book = await Book.findByPk(req.params.id);
+//   await book.destroy();
+//   res.redirect("/books" + book.id);
+// }))
 
 module.exports = router;
